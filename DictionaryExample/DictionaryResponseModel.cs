e9fbb49558ed142adeaf2b7f0c3dc2a0e39dcceb
TMinch <Trent.Minch@eblusolutions.com>
2022-10-19 20:01:07 -0400

    Add project files.

---
+++ DictionaryExample/DictionaryResponseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    internal class DictionaryResponseModel
+    {
+        public string Word { get; set; }
+        public string Phonetic { get; set; }
+        public Phonetic[] Phonetics { get; set; }
+        public string Origin { get; set; }
+        public Meaning[] Meanings { get; set; }
+    }
+
+    public class Phonetic
+    {
+        public string Text { get; set; }
+        public string Audio { get; set; }
+    }
+
+    public class Meaning
+    {
+        public string PartOfSpeech { get; set; }
+        public DefinitionModel[] Definitions { get; set; }
+    }
+
+    public class DefinitionModel
+    {
+        public string Definition { get; set; }
+        public string Example { get; set; }
+        public object[] Synonyms { get; set; }
+        public object[] Antonyms { get; set; }
+    }   
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,60 @@
+﻿
+//API Interaction Setup
+using DictionaryExample;
+using System.Text.Json;
+
+string baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+HttpClient client = new HttpClient
+{
+    BaseAddress = new Uri(baseUrl)
+};
+
+Console.WriteLine("Enter a word to receive the definition");
+Console.WriteLine("Enter 'q' to quit");
+var userInput = Console.ReadLine();
+
+
+while(userInput != "q")
+{
+    var response = await client.GetAsync(userInput);
+    var content = await response.Content.ReadAsStringAsync();
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+
+    List<DictionaryResponseModel> definitionInfoList;
+    try
+    {
+        definitionInfoList = JsonSerializer.Deserialize<List<DictionaryResponseModel>>(content, options);
+    }
+    catch(JsonException)
+    {
+        Console.WriteLine("Not a real word sorry");
+        Console.WriteLine("Enter a word to receive the definition");
+        Console.WriteLine("Enter 'q' to quit");
+        userInput = Console.ReadLine();
+        continue;
+    }
+    
+
+    Console.WriteLine($"Word: {definitionInfoList.FirstOrDefault().Word}");
+
+    foreach (var definitionInfo in definitionInfoList)
+    {
+        foreach(var meaning in definitionInfo.Meanings)
+        {
+            var partOfSpeech = meaning.PartOfSpeech;
+            foreach(var definition in meaning.Definitions)
+            {
+                Console.WriteLine($"{partOfSpeech} : {definition.Definition}");
+            }
+        }
+    }    
+
+    Console.WriteLine("Enter a word to receive the definition");
+    Console.WriteLine("Enter 'q' to quit");
+    userInput = Console.ReadLine();
+}
+ 
